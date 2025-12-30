b557f78f9d42902b14c9267589d4319985e629eb
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2025-12-30 15:14:09 +0200

    Create a function to display books from user's library

--- script.js
+++ script.js
@@ -30,3 +30,49 @@ add.addEventListener('click', () => {
 close.addEventListener('click', () => {
     dialog.close();
 })
+
+addBookToLibrary('testBook', 'testAuthor', 3, false);
+addBookToLibrary('testBook2', 'testAuthor2', 55, true);
+
+let bookShelf = document.querySelector('.content');
+
+myLibrary.forEach(function (book){
+    displayBooks(book);
+})
+
+function displayBooks(book) {
+    let holderDiv = document.createElement('div');
+    holderDiv.classList.add("book")
+
+    let topDiv = document.createElement('div');
+    topDiv.classList.add('top-part')
+        let titleDiv = document.createElement('div');
+        titleDiv.textContent = book.name;
+
+        let buttonDiv = document.createElement('div');
+        buttonDiv.classList.add('book-controls');
+            let readButton = document.createElement('button');
+            book.read ? readButton.classList.add('read') : readButton.classList.add('unread');
+            let modifyButton = document.createElement('button');
+            modifyButton.classList.add('modify');
+            buttonDiv.appendChild(readButton);
+            buttonDiv.appendChild(modifyButton);
+
+    topDiv.appendChild(titleDiv);
+    topDiv.appendChild(buttonDiv);
+
+    let bottomDiv = document.createElement('div');
+    bottomDiv.classList.add('bottom-part');
+        let authorDiv = document.createElement('div');
+        authorDiv.textContent = book.author;
+        let pagesDiv = document.createElement('div');
+        pagesDiv.textContent = book.pages;
+
+    bottomDiv.appendChild(authorDiv);
+    bottomDiv.appendChild(pagesDiv);
+
+    holderDiv.appendChild(topDiv);
+    holderDiv.appendChild(bottomDiv);
+
+    bookShelf.appendChild(holderDiv);
+}
